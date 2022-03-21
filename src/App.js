9a4c5a34f106fb187c81407630bb033302747502
den413 <92449901+den413@users.noreply.github.com>
2022-03-21 20:38:11 +0900

    molecules apply

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 import "./styles.css";
 
 export const App = () => {
@@ -7,6 +8,8 @@ export const App = () => {
     <div>
       <PrimaryButton>テスト</PrimaryButton>
       <SecondryButton>検索</SecondryButton>
+      <br />
+      <SearchInput></SearchInput>
     </div>
   );
 };

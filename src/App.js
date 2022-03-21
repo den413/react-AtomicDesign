de849a4dc6b81f38991f35853425b1ac517eafd0
den413 <92449901+den413@users.noreply.github.com>
2022-03-21 21:30:22 +0900

    organisms apply

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/use/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "toru",
+  image: "https://source.unsplash.com/WX4i1Jq_o0Y",
+  email: "adg@fgef.com",
+  phone: "000-9354-8216",
+  company: {
+    name: "aaaaa会社"
+  },
+  website: "https://google.com"
+};
 
 export const App = () => {
   return (
@@ -9,7 +21,8 @@ export const App = () => {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondryButton>検索</SecondryButton>
       <br />
-      <SearchInput></SearchInput>
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 };
